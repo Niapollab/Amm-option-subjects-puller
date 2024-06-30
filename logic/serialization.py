485df7356df714943c404581ec9e841a8138eab7
Lizzy <104323284+LizzyLiss@users.noreply.github.com>
2024-07-01 01:42:49 +0300

    implemented serialize_report_to_excel method

--- logic/serialization.py
+++ logic/serialization.py
@@ -32,6 +32,18 @@ def deserialize_report(df_report: pd.DataFrame) -> Report:
 
     return Report(student_map)
 
+def auto_adjust_column_width(ws):
+    for col in ws.columns:
+        max_length = 0
+        column = col[0].column_letter 
+        for cell in col:
+            try:
+                if len(str(cell.value)) > max_length:
+                    max_length = len(cell.value)
+            except:
+                pass
+        adjusted_width = (max_length + 2)
+        ws.column_dimensions[column].width = adjusted_width
 
 async def serialize_report_to_excel(filename: str, report: Report) -> None:
     """Serialize a Report object into an Excel file.
@@ -40,5 +52,11 @@ async def serialize_report_to_excel(filename: str, report: Report) -> None:
         filename (str): The path to the file where the report will be saved.
         report (Report): The Report object containing the data to be serialized.
     """
-
-    ...
+    with pd.ExcelWriter(filename, engine='openpyxl') as writer:
+        for group, students in report.groups.items():
+            df = pd.DataFrame([{"ФИО": student.fullname, "Предмет": student.subject} for student in students])
+            df = df.sort_values(by="ФИО")
+            df.to_excel(writer, sheet_name=str(group), index=False)
+            
+            worksheet = writer.sheets[str(group)]
+            auto_adjust_column_width(worksheet)
