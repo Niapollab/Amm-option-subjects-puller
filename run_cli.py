dff20730d199cce70f8f302b51844bf72ca48ee2
Niapoll <Kolya21082001@gmail.com>
2026-08-31 18:09:21 +0300

    Use UV as package manager

--- run_cli.py
+++ run_cli.py
@@ -1,11 +1,12 @@
 from asyncio import run
+
 from cli.cli import CLI, parse_arguments
 
 
-async def main() -> None:
+def main() -> None:
     cli = CLI(parse_arguments())
-    await cli.run_cli()
+    run(cli.run_cli())
 
 
 if __name__ == "__main__":
-    run(main())
+    main()
